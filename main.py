e7d95c8a2f004308e588d21dfdc81d83a901317b
vlzcv <vlada.zaytseva.2009@mail.ru>
2024-10-12 21:15:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-age = input("Введите ваш возраст: ")
-count = 0
-while count < int(age):
-  count += 1
-  print(age)
-
-
+name = input("Введите ваше имя: ")
+age = int(input("Введите ваш возраст: "))
+favorite_book = input("Название твоей любимой книги: ")
+print("Привет,",name,"!","Тебе",age,"лет,","и твоя любимая книга -",favorite_book)
